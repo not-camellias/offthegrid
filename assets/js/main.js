1a3b91912d4afc75144ea3f2d71e570a2979c8d8
not-camille <cgcoopwood@gmail.com>
2026-03-24 17:31:32 +0000

    part of 6.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,29 @@
 
 // console.log("Hello, World!");
 
-let rotation =0;
-document.addEventListener("click", function () {
-    let min = 10;
-    let max = 40;
-rotation += Math.floor(Math.random() * (max-min+1))+min;
-document.body.style.transform = `rotate(${rotation}deg)`;
-});
+//let rotation =0;
+//document.addEventListener("click", function () {
+    //let min = 10;
+   // let max = 40;
+//rotation += Math.floor(Math.random() * (max-min+1))+min;
+//rotation += randomNumber(1,200);
+//document.body.style.transform = `rotate(${rotation}deg)`;
+//})
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max-min) +1) +min;
+}
+function randomize () {
+    divs.forEach(function(div){
+        let scale = randomNumber(.5, 1.2);
+        let translateX = randomNumber (0, 50);
+        let translateY = randomNumber (0,20);
+        let rotate = randomNumber (0, 360);
+        div.style.transform =
+            `scale(${scale})`
+            translate
+    
+    console.log(divs.length, "randomize!")
+}
+
+randomize();
